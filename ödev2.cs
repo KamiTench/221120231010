ea5967501249effb6d32835d64a65d20b2ec5430
KamiTench <167817401+KamiTench@users.noreply.github.com>
2024-10-01 13:17:02 -0700

    Add files via upload

---
+++ ödev2.cs
@@ -0,0 +1,8 @@
+﻿int bakiye = 10000;
+Console.WriteLine("Bakiyeniz: " + bakiye);
+
+Console.Write("Ne kadar çekmek istiyorsun? ");
+int cekim = Convert.ToInt32(Console.ReadLine());
+
+int sonuc = bakiye - cekim;
+Console.WriteLine("Kalan Bakiye: " + sonuc);
